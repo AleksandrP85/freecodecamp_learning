a7d51f65cf9468c1472320d05873f178bcac5855
Aleksandr <aptezh@gmail.com>
2025-04-27 17:04:07 +0300

    JS task

---
+++ script.js
@@ -0,0 +1,34 @@
+//step 22
+
+rows[rows.length -1];
+
+
+
+step 24
+
+let cities = ["London","New York","Mumbai"];
+console.log(cities);
+cities[2] = 'Mexico City';
+//cities[cities.length-1] = 'Mexico Citi';
+console.log(cities);
+
+step 26
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push('freeCodeCamp');
+console.log(rows);
+
+step 27
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+let popped = rows.pop();
+console.log(popped);
+
+step 28
+
+let pushed = rows.push("freeCodeCamp");
+console.log(pushed);
+
+step 32
+
+let pushed = rows.push("freeCodeCamp");
+console.log(pushed);ß
