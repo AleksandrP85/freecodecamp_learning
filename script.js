1a7ab41b36eb266d6723e51b25885704dbdf8790
Aleksandr <aptezh@gmail.com>
2025-04-29 23:12:38 +0300

    Елка

--- script.js
+++ script.js
@@ -1,59 +1,74 @@
 //step 22
 
-rows[rows.length -1];
+// rows[rows.length -1];
 
 
 
-step 24
+// step 24
 
-let cities = ["London","New York","Mumbai"];
-console.log(cities);
-cities[2] = 'Mexico City';
-//cities[cities.length-1] = 'Mexico Citi';
-console.log(cities);
+// let cities = ["London","New York","Mumbai"];
+// console.log(cities);
+// cities[2] = 'Mexico City';
+// //cities[cities.length-1] = 'Mexico Citi';
+// console.log(cities);
 
-step 26
-let rows = ["Naomi", "Quincy", "CamperChan"];
-rows.push('freeCodeCamp');
-console.log(rows);
+// step 26
+// let rows = ["Naomi", "Quincy", "CamperChan"];
+// rows.push('freeCodeCamp');
+// console.log(rows);
 
-step 27
-let rows = ["Naomi", "Quincy", "CamperChan"];
-rows.push("freeCodeCamp");
-let popped = rows.pop();
-console.log(popped);
+// step 27
+// let rows = ["Naomi", "Quincy", "CamperChan"];
+// rows.push("freeCodeCamp");
+// let popped = rows.pop();
+// console.log(popped);
 
-step 28
+// step 28
 
-let pushed = rows.push("freeCodeCamp");
-console.log(pushed);
+// let pushed = rows.push("freeCodeCamp");
+// console.log(pushed);
 
-step 32
+// step 32
 
-let pushed = rows.push("freeCodeCamp");
-console.log(pushed);
+// let pushed = rows.push("freeCodeCamp");
+// console.log(pushed);
 
-step 36
+// step 36
+// for (let i = 0; i < count; i = i + 1) {
+
+// }
+
+// step 37
+
+// rows.push(i);
+
+// step 39
+
+// let result = "";
+//Елка
+const character = "*";
+const count = 8;
+const rows = [];
 for (let i = 0; i < count; i = i + 1) {
-
+  rows.push(character.repeat(i + 1))
 }
 
-step 37
+let result = ""
 
-rows.push(i);
+for (const row of rows) {
+  result = result + row + "\n";
+}
 
-step 39
+console.log(result);
 
-let result = "";
+// step 46
+// for (let i = 0; i < count; i = i + 1) {
+//     rows.push(character.repeat(i + 1))
+//   }
 
-step 46
-for (let i = 0; i < count; i = i + 1) {
-    rows.push(character.repeat(i + 1))
-  }
+// step 47 
 
-step 47 
+// function padRow(){
 
-function padRow(){
-
-} 
+// } 
 
